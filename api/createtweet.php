0f5f2f29b7b6a02d4b55d4b8d65e42894df23564
bppalomer <bppalomer@gmail.com>
2023-06-25 17:03:27 +0800

    Added some additional functions

--- api/createtweet.php
+++ api/createtweet.php
@@ -7,9 +7,10 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
 
     $content = $data['content'];
+    $date_tweeted = $data['date_tweeted'];
     $user_id = $data['user_id'];
 
-    $sql = "INSERT INTO posts (content, user_id) VALUES ('$content','$user_id')";
+    $sql = "INSERT INTO tweets (content,date_tweeted,user_id) VALUES ('$content','$date_tweeted','$user_id')";
     if($conn->query($sql)){
         $response = array (
             'success' => true,
